36236cc41c7e27a6d5df9d540988aca7965aa801
jesnan <jsnnarayan@gmail.com>
2024-05-01 20:46:12 +0530

    footer

--- src/components/Dilfooter.js
+++ src/components/Dilfooter.js
@@ -32,110 +32,44 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
       </section>
 
       <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                <a href='#!' className='text-reset'>
-                About Us
-                </a>
-              </h6>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                <a href='#!' className='text-reset'>
-                Terms&Condition
-                </a>
-                
-              </h6>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                <a href='#!' className='text-reset'>
-                F & Q
-                </a>
-              </h6>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                <a href='#!' className='text-reset'>
-                Return Policy
-                </a>
-              </h6>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                <a href='#!' className='text-reset'>
-                Refund Policy
-                </a>
-              </h6>
-            {/* </MDBCol>
+  <MDBContainer className='text-center text-md-start mt-5'>
+    <MDBRow className='mt-3'>
+      <MDBCol md="3" lg="4" xl="3" className='mb-4'> {/* Remove mx-auto class */}
+        <h6 className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          <a href='#!' className='text-reset'>
+            About Us
+          </a>
+        </h6>
+        <h6 className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          <a href='#!' className='text-reset'>
+            Terms&Condition
+          </a>
+        </h6>
+        <h6 className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          <a href='#!' className='text-reset'>
+            F & Q
+          </a>
+        </h6>
+        <h6 className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          <a href='#!' className='text-reset'>
+            Return Policy
+          </a>
+        </h6>
+        <h6 className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          <a href='#!' className='text-reset'>
+            Refund Policy
+          </a>
+        </h6>
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
+</section>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Terms&Condition</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  React
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
-              </p>
-              <p>
-                <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
-              </p> */}
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
 
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
         © 2024 Copyright:
